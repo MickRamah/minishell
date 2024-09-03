2276caac2bf7b83beb4fa0f2703b31d78a51590c
Zonirina Mick Ramahazomanana <zramahaz@c1r13s7.42antananarivo.mg>
2024-09-03 15:58:22 +0300

    ajout de plusieurs bibliotheque dans minishell.h

--- lib/minishell.h
+++ lib/minishell.h
@@ -6,7 +6,7 @@
 /*   By: zramahaz <zramahaz@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 16:48:17 by zramahaz          #+#    #+#             */
-/*   Updated: 2024/08/30 16:49:17 by zramahaz         ###   ########.fr       */
+/*   Updated: 2024/09/03 11:52:44 by zramahaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 # define MINISHELL_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #endif
