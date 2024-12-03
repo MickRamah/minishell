67825bc9101fe56da15d0bbd6206334126e6a99a
Herintsoa Christian Rakotonirina <herakoto@c1r5s13.42antananarivo.mg>
2024-12-03 18:45:03 +0300

    add include + separation

--- include/function.h
+++ include/function.h
@@ -6,14 +6,23 @@
 /*   By: herakoto <herakoto@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 13:51:43 by herakoto          #+#    #+#             */
-/*   Updated: 2024/11/27 13:53:28 by herakoto         ###   ########.fr       */
+/*   Updated: 2024/12/03 18:37:54 by herakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FUNCTION_H
 # define FUNCTION_H
 
+# include "types.h"
+
 int	count_arg(char **arg);
+
+// signal handling
+void	ft_end_of_file(t_data *data);
+void	ft_signal_handler();
+
+//utils
+int		ft_free_var(t_data *data);
 
 #endif
 
