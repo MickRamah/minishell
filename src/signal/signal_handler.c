63eab451fd998c20d89f55d1a5228fb21414c608
Herintsoa Christian Rakotonirina <herakoto@c1r13s11.42antananarivo.mg>
2024-12-06 16:07:13 +0300

    fix signal ctrl+c

--- src/signal/signal_handler.c
+++ src/signal/signal_handler.c
@@ -6,25 +6,35 @@
 /*   By: herakoto <herakoto@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:44:06 by herakoto          #+#    #+#             */
-/*   Updated: 2024/12/02 17:58:02 by herakoto         ###   ########.fr       */
+/*   Updated: 2024/12/06 16:06:43 by herakoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	ft_sigint_function(int sig)
+void	ft_sigint_function(int sig, siginfo_t *siginfo, void *context)
 {
-	if (sig == SIGINT)
+	int	pid;
+
+	(void)context;
+	pid = siginfo->si_pid;
+	write(1, "\n", 2);
+	if (sig == SIGINT && pid > 0)
 	{
-		write(1, "\n", 2);
+		write(1, "parent\n", 8);
 		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
 	}
 }
 
-void	ft_signal_handler()
+void	ft_signal_handler(void)
 {
-	signal(SIGINT, ft_sigint_function);
+	struct sigaction	s_sa;
+
+	s_sa.sa_sigaction = ft_sigint_function;
+	s_sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&s_sa.sa_mask);
+	sigaction(SIGINT, &s_sa, NULL);
 	signal(SIGQUIT, SIG_IGN);
 }
